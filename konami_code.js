2095778b5e466dcac9449e846d8878b068bbcf0a
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-07 09:00:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,7 @@ document.body.addEventListener('keydown',function(e){
     if (key === code[index]) {
       index++;
 
-      if (index === alphabet.length) {
+      if (index === code.length) {
         alert("Hurray!");
 
         index = 0;
